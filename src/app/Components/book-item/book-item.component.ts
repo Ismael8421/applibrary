8e6776c93eea4b99be5420aa4d6b522eac054987
ismaelparapi40 <ismaelparapi40@gmail.com>
2024-12-18 16:07:12 +0000

    first commit

---
+++ src/app/Components/book-item/book-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, inject } from '@angular/core';
+import { IBook } from '../../Models/book.mode'
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-book-item',
+  imports: [],
+  templateUrl: './book-item.component.html',
+  styleUrl: './book-item.component.css'
+})
+export class BookItemComponent {
+  route = inject(Router);
+  @Input() item!:IBook;
+  navigateToID() {
+    this.route.navigate(['books', this.item.id]);
+  }
+}
---
+++ src/app/Services/book.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { IBook } from '../Models/book.mode';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookService {
+  listaLibros: IBook[] =[
+    { id: 1, tittle: 'El Principito', author: 'Antoine de Saint-Exupéry' },
+    { id: 2, tittle: '1984', author: 'George Orwell' },
+    { id: 3, tittle: 'Cien años de soledad', author: 'Gabriel García Márquez' },
+  ];
+
+  constructor() {}
+
+  getBooks(): IBook[] {
+    return this.listaLibros;
+  } 
+
+  getBookById(id: number): IBook | undefined {
+    for (let i = 0; i < this.listaLibros.length; i++){
+      if  (this.listaLibros[i].id === id){
+        return this.listaLibros[i];
+      }
+    }
+    return undefined;
+  }
+}
